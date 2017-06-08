35000f3b29024313735e2bd512a1fb9bb63a0514
gieron.krzysztof <gieron.krzysztof@gmail.com>
2017-06-09 00:01:22 +0200

    dodanie wykresow, tabow

--- js/funkcje.js
+++ js/funkcje.js
@@ -1,4 +1,6 @@
-var table;
+var calculationResultTable;
+var calculationSummaryTable;
+var detailsChart;
 
 (function () {
   var app = angular.module('myApp', []);
@@ -29,19 +31,172 @@ var table;
       else {
         interestRate = $scope.InterestRate;
       }
+      var schedule;
       if ($scope.installmentType === "FixedRate") {
-        var schedule = calculateFixedInstallmentSchedule(parseFloat($scope.creditAmount), (parseFloat($scope.spread) + parseFloat(interestRate)) / 100, $scope.creditTime);
-        drawTable(schedule);
+        schedule = calculateFixedInstallmentSchedule(parseFloat($scope.creditAmount), (parseFloat($scope.spread) + parseFloat(interestRate)) / 100, $scope.creditTime);
+        drawCalculationResultTable(schedule);
       }
       else if ($scope.installmentType === "VariableRate") {
-        var schedule = calculateVariableInstallmentSchedule(parseFloat($scope.creditAmount), (parseFloat($scope.spread) + parseFloat(interestRate)) / 100, $scope.creditTime);
-        drawTable(schedule);
+        schedule = calculateVariableInstallmentSchedule(parseFloat($scope.creditAmount), (parseFloat($scope.spread) + parseFloat(interestRate)) / 100, $scope.creditTime);
+        drawCalculationResultTable(schedule);
       }
+      var interestsInTime = GetInterestsInTime(schedule);
+      var capitalInTime = GetCapitalInTime(schedule);
+      var remainingCapitalInTime = GetRemainingCapitalInTime(schedule);
+      var installmentsInTime = GetInstallmentsInTime(schedule);
+
+      var labels = GenerateLablesForChart(remainingCapitalInTime);
+      DrawInstallmentDetailsChart(interestsInTime, capitalInTime, installmentsInTime, remainingCapitalInTime, labels);
+      DrawRemainingCapitalInTimeChart(remainingCapitalInTime, labels);
+      var creditSummary = GetCreditSummary($scope.creditAmount, schedule, $scope.commision, $scope.spread, interestRate, $scope.estateValue);
+      drawCalculationSummaryTable(creditSummary);
     };
 
 
   });
 })();//<-- here
+
+function GetInstallmentsInTime(schedule) {
+  var installmentsInTime = [];
+  schedule.forEach(function (element) {
+    installmentsInTime.push(element[1]);
+  }, this);
+  return installmentsInTime;
+}
+
+function GetInterestsInTime(schedule) {
+  var interestsInTime = [];
+  schedule.forEach(function (element) {
+    interestsInTime.push(element[3]);
+  }, this);
+  return interestsInTime;
+}
+
+function GetCapitalInTime(schedule) {
+  var interestsInTime = [];
+  schedule.forEach(function (element) {
+    interestsInTime.push(element[2]);
+  }, this);
+  return interestsInTime;
+}
+
+function GetRemainingCapitalInTime(schedule) {
+  var remainingCapitalInTime = [];
+  schedule.forEach(function (element) {
+    remainingCapitalInTime.push(element[4]);
+  }, this);
+  return remainingCapitalInTime;
+}
+
+function GenerateLablesForChart(arrayInTime) {
+  var labels = [];
+  for (var i = 1; i <= arrayInTime.length; i++) {
+    // if (i === 1 || i%5 == 0 || i === arrayInTime.length){
+    labels.push(i.toString());
+    // }
+    //  else {
+    //    labels.push("");
+    //  }
+
+  }
+  return labels;
+}
+
+function DrawInstallmentDetailsChart(interestsInTime, capitalInTime, installmentsInTime, remainingCapitalInTime, labels) {
+  var ctx = document.getElementById('installmentDetailsChart').getContext('2d');
+if (detailsChart != undefined){
+  detailsChart.destroy();
+}
+
+  detailsChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+
+      labels: labels,
+      datasets: [{
+        label: 'Odsetki',
+        data: interestsInTime,
+        yAxisID: "y-axis-0",
+        backgroundColor: "rgba(153,255,51,0.4)"
+      }, {
+        label: 'Kapitał',
+        data: capitalInTime,
+        yAxisID: "y-axis-0",
+        backgroundColor: "rgba(255,153,0,0.4)"
+      }, {
+        label: 'Rata',
+        data: installmentsInTime,
+        yAxisID: "y-axis-0",
+        backgroundColor: "rgba(55,153,0,0.4)"
+      }, {
+        label: 'Kapitał do spłaty',
+        data: remainingCapitalInTime,
+        yAxisID: "y-axis-1",
+        backgroundColor: "rgba(240,53,55,0.4)"
+      }]
+    },
+    options: {
+      responsive: true,
+
+// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+
+maintainAspectRatio: true,
+      scales: {
+        yAxes: [{
+          position: "left",
+          "id": "y-axis-0",
+          fontSize: 50
+        }, {
+          position: "right",
+          "id": "y-axis-1"
+        }]
+      }
+    }
+  });
+}
+
+function DrawRemainingCapitalInTimeChart(remainingCapitalInTime, labels) {
+  var ctx = document.getElementById('remainingCapitalInTimeChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Kapitał do spłaty',
+        data: remainingCapitalInTime,
+        backgroundColor: "rgba(55,153,0,0.4)"
+      }],
+    },
+    xAxes: [{
+      display: false
+    }]
+  });
+}
+
+function GetCreditSummary(amount, schedule, commissionPercent, spread, interestRate, estateValue) {
+  var result = [];
+  var sumInterestsToPay = 0;
+  var creditAmount = parseFloat(amount);
+  var commisionToPay = Math.round(parseFloat(amount) * ((parseFloat(commissionPercent) / 100) * 100)) / 100;
+  var creditRate = parseFloat(spread) + parseFloat(interestRate);
+  var ltv = Math.round(parseFloat(amount) / parseFloat(estateValue) * 100);
+  schedule.forEach(function (element) {
+    sumToPay += element[1];
+    sumInterestsToPay += element[3];
+  });
+  var sumToPay = Math.round((creditAmount + sumInterestsToPay + commisionToPay) * 100) / 100;
+
+  result.push({
+    0: creditAmount,
+    1: sumToPay,
+    2: sumInterestsToPay,
+    3: commisionToPay,
+    4: creditRate,
+    5: ltv
+  });
+
+  return result;
+}
 
 function GetInterestRateFromApi(rateTime, currency) {
   var rateUrl = GetCorrectUrl(currency);
@@ -50,19 +205,19 @@ function GetInterestRateFromApi(rateTime, currency) {
   var currencyJson = $.ajax({
     url: rateUrl,
     context: document.body,
-    async: false 
+    async: false
   }).done(function (x) {
     response = JSON.parse(x);
-    
+
   });
   var a = GetObjectFromArrayMeetCondition(response, rateTime, currency);
   return parseFloat(a.value.replace(/ *\([^)]*\) */g, "").replace(/,/, "."));
 }
 
-function GetObjectFromArrayMeetCondition(array, rateTime, currency){
+function GetObjectFromArrayMeetCondition(array, rateTime, currency) {
   var result;
   array.filter(function (obj) {
-    if (obj.rateName ==  currency + " " + rateTime){
+    if (obj.rateName == currency + " " + rateTime) {
       result = obj;
     }
   });
@@ -134,12 +289,12 @@ function calculateFixedInstallmentSchedule(amount, rate, months) {
   return installmentsDetails;
 }
 
-function drawTable(tableData) {
-  if (table !== undefined) {
-    table.destroy();
+function drawCalculationResultTable(tableData) {
+  if (calculationResultTable !== undefined) {
+    calculationResultTable.destroy();
   }
 
-  table = $('#calculationResult').DataTable({
+  calculationResultTable = $('#calculationResult').DataTable({
     data: tableData,
     columns: [
       { title: "Nr raty" },
@@ -163,7 +318,38 @@ function drawTable(tableData) {
   });
 }
 
+function drawCalculationSummaryTable(tableData) {
+  if (calculationSummaryTable !== undefined) {
+    calculationSummaryTable.destroy();
+  }
 
+  calculationSummaryTable = $('#calculationSummary').DataTable({
+    data: tableData,
+    "bPaginate": false,
+    columns: [
+      { title: "Kwota kredytu" },
+      { title: "Całkowita kwota do spłaty" },
+      { title: "Koszt odsetek" },
+      { title: "Koszt prowizji" },
+      { title: "Oprocentowanie" },
+      { title: "LTV" }
+    ],
+    "info": false,
+    responsive: true,
+    "language": {
+      "lengthMenu": "Wyświetl _MENU_ rekordów na stronę",
+      "zeroRecords": "Nic nie znaleziono",
+      "info": "Pokazywanie strony _PAGE_ z _PAGES_",
+      "infoEmpty": "Brak rekordów",
+      "infoFiltered": "(filtered from _MAX_ total records)",
+      "search": "Szukaj"
+    },
+    searching: false,
+    "columnDefs": [
+      { "className": "dt-center", "targets": "_all" }
+    ]
+  });
+}
 
 
 $(document).ready(function () {
